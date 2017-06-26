c6116d1f1b2ca3bd5e0dac522e33aabab247b66c
Kurt Wheeler <kurt.wheeler91@gmail.com>
2017-06-26 12:39:17 -0400

    Changes the way deploys happen to prevent overutilization of resources.

--- container-registry.tf
+++ container-registry.tf
@@ -60,7 +60,7 @@ resource "aws_ecs_service" "cognoma-core-service" {
   task_definition = "${aws_ecs_task_definition.cognoma-core-service.arn}"
   desired_count  = 2
   deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent = 200
+  deployment_maximum_percent = 100
   iam_role = "${aws_iam_role.ecs-service-role.name}"
   depends_on = ["aws_iam_role_policy.ecs-service"]
 
@@ -89,7 +89,7 @@ resource "aws_ecs_service" "nginx" {
   task_definition = "${aws_ecs_task_definition.cognoma-nginx.arn}"
   desired_count  = 2
   deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent = 200
+  deployment_maximum_percent = 100
   iam_role = "${aws_iam_role.ecs-service-role.name}"
   depends_on = ["aws_iam_role_policy.ecs-service"]
 
